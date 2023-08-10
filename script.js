87edfec68d25643449f0c4f9d00250741f7496c2
emran <emranbspt@gmail.com>
2023-08-10 22:54:27 +0600

    add some number theory functions

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+mp={}
+sv = ()=>{
+
+    for(i=2; i*i<=10000000;i++){
+        if(mp[i]==undefined){
+            for(j=i+i; j<=10000000;j+=i){
+                mp[j]=1
+            }
+        }
+    }
+}
+
+
+primeDivisor = (val)=>{
+    ar=[]
+    ind=0
+    for(i=1; i*i<=val; i++){
+        if(val%i==0){
+            ar[ind++]=i;
+            if(i!=val/i)ar[ind++]=val/i
+        }
+    }
+    ar.sort((a,b)=>a-b);
+    return ar
+}
+findPrimes = (l,r)=>{
+    ar=[]
+    sv();
+    ind=0
+    for(i=2; i<=r; i+=2){
+        if(mp[i]==undefined){
+            ar[ind++]=i;
+        }
+    }
+    return ar
+}
+
+
 $('#inp').on('change',()=>{
     
     fVals = $('#inp').val();
@@ -20,9 +58,35 @@ $('#inp').on('change',()=>{
     }
     vals = vals.trim();
     arr = vals.split(" ");
+    
+    mn=999999999999999
+    mx=-999999999999999
     for(var i=0; i<arr.length; i++){
-        arr[i]=parseInt(arr[i]*100)
+        arr[i]=parseInt(arr[i])
+        
+        mn=Math.min(mn,arr[i])
+        mx=Math.max(mx,arr[i])
     }
-    console.log(arr)
+    if(flag){
+        if(arr[0]>arr[1]){
+            [arr[0], arr[1]]=[arr[1],arr[0]]
+        }
+        ind=0;
+        r=arr[1]
+        for(l=arr[0]; l<=r; l++){
+            arr[ind++]=l;
+            mn=Math.min(mn,arr[ind])
+            mx=Math.max(mx,arr[ind])
+        }
+    }
+    console.log(arr,mn,mx)
+    for(var i=0; i<arr.length; i++){
+      //  console.log(arr[i],': ',primeDivisor(arr[i]))
+    }
+    //sv();
+    if(flag){
+        primes = findPrimes(arr[0],arr[1]);
+        console.log('primes',primes)
+    }
 
 })
